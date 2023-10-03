92e22b0c6cfe2d7c9a97dce727d727ae1e6aa901
Billy Ponto <bpgponto@gmail.com>
2023-10-03 09:47:38 +0800

    yta nusantara landing page

--- script.js
+++ script.js
@@ -70,6 +70,16 @@ filterInput.addEventListener("input", (e) => {
 
 const links = [
 	{
+		name: "YTA Nusantara Landing Page",
+		githubRepo: "yta-nusantara-html",
+		icons: [
+			{ name: "fa-brands fa-html5", color: "text-orange-600" },
+			{ name: "javascript", color: "" },
+			{ name: "tailwindcss", color: "" },
+		],
+		liveSiteLink: "https://yta-nusantara.netlify.app/",
+	},
+	{
 		name: "Tickety Landing Page",
 		githubRepo: "tickety-html",
 		icons: [
